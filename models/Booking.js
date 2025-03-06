10dba0664307bcc82680baa8d215dc5e84b0c319
e1enax <elenasamuel6@gmail.com>
2025-03-06 19:51:06 +0200

    booking.js added

---
+++ models/Booking.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  event: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Event',
+    required: true
+  },
+  numberOfTickets: {
+    type: Number,
+    required: true,
+    min: 1
+  },
+  totalPrice: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'confirmed', 'canceled'],
+    default: 'pending'
+  }
+}, { 
+  timestamps: true,
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true }
+});
+const Booking = mongoose.model('Booking', bookingSchema);
+
+module.exports = Booking;
---
+++ models/Event.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    ticketPricing: {
+        type: Number,
+        required: true
+    },
+    totalTickets: {
+        type: Number,
+        required: true
+    },
+    remainingTickets: {
+        type: Number,
+        required: true
+    },
+    organizer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Event', eventSchema);
+
