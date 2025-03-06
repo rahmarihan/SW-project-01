9a967faf5e6f1b82d373316da315bd2cb377632b
e1enax <elenasamuel6@gmail.com>
2025-03-06 22:09:53 +0200

    edited event

--- models/Booking.js
+++ models/Booking.js
@@ -11,26 +11,5 @@ const bookingSchema = new mongoose.Schema({
     ref: 'Event',
     required: true
   },
-  numberOfTickets: {
-    type: Number,
-    required: true,
-    min: 1
-  },
-  totalPrice: {
-    type: Number,
-    required: true,
-    min: 0
-  },
-  status: {
-    type: String,
-    enum: ['pending', 'confirmed', 'canceled'],
-    default: 'pending'
-  }
-}, { 
-  timestamps: true,
-  toJSON: { virtuals: true },
-  toObject: { virtuals: true }
-});
-const Booking = mongoose.model('Booking', bookingSchema);
+ });
 
-module.exports = Booking;
--- models/Event.js
+++ models/Event.js
@@ -1,52 +1,10 @@
 const mongoose = require('mongoose');
 
-const eventSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    date: {
-        type: Date,
-        required: true
-    },
-    location: {
-        type: String,
-        required: true
-    },
-    category: {
-        type: String,
-        required: true
-    },
-    image: {
-        type: String,
-        required: true
-    },
-    ticketPricing: {
-        type: Number,
-        required: true
-    },
-    totalTickets: {
-        type: Number,
-        required: true
-    },
-    remainingTickets: {
-        type: Number,
-        required: true
-    },
-    organizer: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
-});
+const eventSchema = new mongoose.Schema({},
+    
+    { timestamps: true }
+
+);
 
 module.exports = mongoose.model('Event', eventSchema);
 
