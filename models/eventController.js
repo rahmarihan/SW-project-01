985b214fe28ab260b08ee6d53b614e463fa22923
elena <elenasamuel6@gmail.com>
2025-04-14 18:35:35 +0200

    trying to add eventController

--- models/eventController.js
+++ models/eventController.js
@@ -1,4 +1,4 @@
-const Event = require('../models/Event');
+const Event = require('./Event');
 const ErrorResponse = require('../utils/errorResponse');
 
 // Part C.1 - Organizer gets their own events
