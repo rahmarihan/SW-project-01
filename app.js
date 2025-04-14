e311917fde9c23e4dd639037f9c8a79846f98f93
elena <elenasamuel6@gmail.com>
2025-04-14 17:55:22 +0200

    hi

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000c;
 app.listen(PORT, () => 
   console.log(`Server running on port ${PORT} | http://localhost:${PORT}`)
 );
