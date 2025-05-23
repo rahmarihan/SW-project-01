d8ab4d13b4908edfbafc06f3cffad321e6191ef2
rahmarihan <rahma.rihan@student.giu-uni.de>
2025-05-23 17:51:49 +0300

    added user, admin, organizer pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,34 +2,65 @@ import { Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import Unauthorized from './components/Unauthorized';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import ProtectedRoute from './components/ProtectedRoute';
 import MyPage from './components/MyPage';
+import AdminPage from './components/AdminPage';
+import OrganizerPage from './components/OrganizerPage';
 
 import LoginForm from './components/LoginForm';
 import RegisterForm from './components/RegisterForm';
 import ForgotPassword from './components/ForgetPassword';
 
-
 function App() {
   return (
-      <>
-        <Navbar />
-        
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/register" element={<RegisterForm />} />
-          <Route path="/forgetPassword" element={<ForgotPassword />} />
-          <Route path="/my-page" element={<MyPage />} />
-          
-        </Routes>
+    <>
+      <Navbar />
 
-        <Footer />
-        <ToastContainer position="top-right" autoClose={3000} pauseOnHover />
-      </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/forgetPassword" element={<ForgotPassword />} />
+        <Route path="/unauthorized" element={<Unauthorized />} />
+
+        {/* User Role Route */}
+        <Route
+          path="/my-page"
+          element={
+            <ProtectedRoute allowedRoles={['user']}>
+              <MyPage />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Admin Route */}
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute allowedRoles={['admin']}>
+              <AdminPage />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Organizer Route */}
+        <Route
+          path="/organizer"
+          element={
+            <ProtectedRoute allowedRoles={['organizer']}>
+              <OrganizerPage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+
+      <Footer />
+      <ToastContainer position="top-right" autoClose={3000} pauseOnHover />
+    </>
   );
 }
 
