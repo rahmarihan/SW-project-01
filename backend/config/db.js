7491172325bbc322b6caaa4bd1631aa27f1b6cf1
rahmarihan <rahma.rihan@student.giu-uni.de>
2025-05-24 21:26:03 +0300

    fixed the mongodb error

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/event_ticketing');
+    const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017';
+    const dbName = process.env.DB_NAME || 'event_ticketing';
+    await mongoose.connect(`${dbUrl}/${dbName}`);
     console.log('MongoDB Connected');
   } catch (err) {
     console.error('MongoDB Connection Error:', err);
